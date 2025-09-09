9cdb7e58bc66ddd4c256b073a5e404714bd6a64a
miniscv <ahdzl126@naver.com>
2025-09-09 12:40:25 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from enum import Enum
+from datetime import datetime
+from typing import List
+
+app = FastAPI()
+
+class Risk(str, Enum):
+    POSITIVE = "POSITIVE"
+    DANGER = "DANGER"
+    CRITICAL = "CRITICAL"
+    EMERGENCY = "EMERGENCY"
+
+class Dialogue(BaseModel):
+    dollId: str
+    type: Risk
+    text: str
+    utteredAt: datetime
+    
+@app.post("/predict/sentiment")
+def predict_sentiment(dialogues: List[Dialogue]):
+    for dialogue in dialogues:
+        print(dialogue)
+    return {
+        "message": f"총 {len(dialogues)}개의 데이터가 성공적으로 수신되었습니다."
+    }
